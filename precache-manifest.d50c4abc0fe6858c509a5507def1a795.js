48513132adc82bdf4de1ffaad78b0a96af0ff7aa
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-05-05 17:59:30 -0400

    Updates

---
+++ precache-manifest.d50c4abc0fe6858c509a5507def1a795.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe5440ac90b70c0847ae4574ea0da5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e069dfc1f1e43545a7c",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "94d7435421d388439101",
+    "url": "/static/css/main.65799222.chunk.css"
+  },
+  {
+    "revision": "2e069dfc1f1e43545a7c",
+    "url": "/static/js/2.3e916300.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3e916300.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94d7435421d388439101",
+    "url": "/static/js/main.3a6165ab.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "b038b6e2277a2240ca0b5fb75443caa2",
+    "url": "/static/media/IMG_0053.b038b6e2.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.86cdd23d3cc81df356787c2395f127e0.js"
+  "/precache-manifest.d50c4abc0fe6858c509a5507def1a795.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

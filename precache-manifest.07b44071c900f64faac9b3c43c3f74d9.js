bed785c1bc361076aee87863b437c671c8d071a3
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-05-05 18:43:09 -0400

    Updates

---
+++ precache-manifest.07b44071c900f64faac9b3c43c3f74d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e71331f8649ca4cb0db73254e4969c70",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2e069dfc1f1e43545a7c",
+    "url": "/portfolio/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "90dd7d1ae4ec881a9ccb",
+    "url": "/portfolio/static/css/main.65799222.chunk.css"
+  },
+  {
+    "revision": "2e069dfc1f1e43545a7c",
+    "url": "/portfolio/static/js/2.3e916300.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.3e916300.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90dd7d1ae4ec881a9ccb",
+    "url": "/portfolio/static/js/main.2c647d37.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "b038b6e2277a2240ca0b5fb75443caa2",
+    "url": "/portfolio/static/media/IMG_0053.b038b6e2.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d50c4abc0fe6858c509a5507def1a795.js"
+  "/portfolio/precache-manifest.07b44071c900f64faac9b3c43c3f74d9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
